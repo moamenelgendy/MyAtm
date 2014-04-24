a28a68f1a5981423fc2ead4ea36c9ed9f93b1c12
unknown <moamen_elgandy2010@yahoo.com>
2014-04-24 23:57:19 +0300

    add api

---
+++ app/controllers/api_controller.rb
@@ -0,0 +1,23 @@
+class ApiController < ApplicationController
+
+
+def getbanks()
+	@banks=Bank.all;
+	respond_to do |format|
+        format.html # index.html.erb
+        format.json { render :json => @banks}
+        end
+  end 
+  
+  def getatm()
+	selectAtm(Integer(params[:bankid]))
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render :json => @query}
+    end
+  end 
+  
+  def selectAtm( bankid =1)
+		@query = Atm.where("bank = #{bankid}") 
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Myatm::Application.routes.draw do
   resources :banks
 
   resources :atms
+  
+    get "api/getbanks"
+	
+	get "api/getatm"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
